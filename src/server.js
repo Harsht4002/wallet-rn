d39860d84042586628507e84d40a1b2313954bf8
Harshwardhan Thube <harsht4002@gmail.com>
2025-06-09 19:47:34 +0530

    fourth commit

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use("/api/transactions", transactionroutes);
 const PORT = process.env.PORT;
 
-app.get("/health", (req, res) => {
+app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "Ok" });
 });
 
